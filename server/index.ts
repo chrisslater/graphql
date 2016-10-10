e889356cf6db0fc7413c937adc3ea4e73039e7cb
Christopher Slater <chris@snapper.fish>
2016-10-10 16:30:50 +0100

    tweak app to work with proxy

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import * as Koa from "koa";
 import * as parser from "koa-bodyparser";
 import * as Router from "koa-router";
+import * as logger from "koa-logger";
 import { apolloKoa, graphiqlKoa } from "apollo-server";
 
 import schema from "../schema";
@@ -9,10 +10,16 @@ const app = new Koa();
 const router = new Router();
 
 app.use(parser());
+app.use(logger());
 
-const endpointURL = "/graphql";
+const endpointURL = "/";
+/**
+ * TODO: This needs to become an environment var so that it will work on both on
+ * local dev and deployed on a proxy url.
+ */
+const viewerEndpointURL = "/graphql/";
 router.post(endpointURL, apolloKoa({ schema }));
-router.get("/", graphiqlKoa({ endpointURL }));
+router.get("/viewer", graphiqlKoa({ endpointURL: viewerEndpointURL }));
 
 app.use(router.routes());
 app.use(router.allowedMethods());
